a5e9dd45612d53e814add0047a078395d7c3b012
Karl Gusner <karlgusner@gmail.com>
2011-08-01 21:20:33 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Loveallogy::Application.config.secret_token = '3cf1a9465df29d8fea47b645898261f5319ace3293298df4cbb0dae21b4c43fabef81f36875188af97b9c296b14d2ab67323de32ec38812ba6d7c0e68d8a8841'
